538d2caf26aa6ff3c930d76cfdcfcc5d3427ce7a
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-25 15:00:18 +0100

    adds a new patch method to control vote requests

--- nc-news/src/components/Api.js
+++ nc-news/src/components/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const getArticles = async (topic) => {
     const {data} = await axios.get("https://nc-news-mdog.herokuapp.com/api/articles")
     return topic ? data.articles.filter(article => article.topic === topic) : data.articles
-
+    // topic as a query rather than a filter
 }
 export const getTopics = async () => {
     const {data} = await axios.get("https://nc-news-mdog.herokuapp.com/api/topics")
@@ -16,4 +16,15 @@ export const getArticle = async (article_id) => {
 export const getArticleComments = async (article_id) => {
     const {data} = await axios.get(`https://nc-news-mdog.herokuapp.com/api/articles/${article_id}/comments`)
     return data.comments
+}
+
+export const getUser = async (username) => {
+    const {data} = await axios.get(`https://nc-news-mdog.herokuapp.com/api/users/${username}`)
+    return data.user
+}
+
+export const updateVotes = async (inc, id, comp) => {
+    const {data} = await axios.patch(`https://nc-news-mdog.herokuapp.com/api/${comp}s/${id}`, {inc_votes: inc})
+    console.log(data[comp], 'api')
+    return data[comp]
 }
