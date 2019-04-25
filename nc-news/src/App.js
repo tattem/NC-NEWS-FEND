217e802c39fdb89239720483f6c727106b460893
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-25 14:55:55 +0100

    improves authorisation

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -5,22 +5,40 @@ import Articles from './components/Articles';
 import Article from './components/Article';
 import Footer from './components/Footer';
 import './App.css';
-// import Content from './components/Content';
+import Auth from './components/Auth';
+import * as api from './components/Api';
 
 
 class App extends Component {
+  state = {
+    user: null,
+    loginFailed: false
+  };
+
   render() {
+    console.log(this.state.user, '<< user')
     return (
-      <div className="App">
-        <Header />
-        <Router className="main">
-          <Articles path="/" />
-          <Articles path="/:topic/articles" />
-          <Article path="/:article" />
-        </Router>
-        <Footer />
-      </div>
+      // <Auth user={this.state.user} login={this.login}>
+        <div className="App">
+          <Header />
+          <Router className="main">
+            <Articles path="/" />
+            <Articles path="/:topic/articles" />
+            <Article path="/:article" />
+          </Router>
+          <Footer />
+        </div>
+      // </Auth>
     );
+  }
+  login = async username => {
+    const user = await api.getUser(username)
+    console.log(user, '<< ran user')
+    this.setState(()=> {
+      return {
+        user: user
+      }
+    })
   }
 }
 
