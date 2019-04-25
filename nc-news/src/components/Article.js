af322b3fa5f3a0146fbb11ccb26c15de17a3a10d
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-25 14:19:24 +0100

    adds voting functionality to an article and its comments

--- nc-news/src/components/Article.js
+++ nc-news/src/components/Article.js
@@ -5,29 +5,95 @@ import * as api from './Api';
 class Article extends Component {
   state = {
     article: {},
-    comments: []
+    comments: [],
+    vote_inc: 0
   };
   render() {
+    const {
+      title,
+      topic,
+      body,
+      author,
+      article_id,
+      votes,
+      comment_count
+    } = this.state.article;
+    console.log(this.state, '< the state');
     return (
       <div className="Content">
         <div className="article">
-          <p>{this.state.article.title}</p>
-          <p>{this.state.article.topic}</p>
-          <p>{this.state.article.body}</p>
-          <p>{this.state.article.author}</p>
-          <p>{this.state.article.votes}</p>
-          <p>{this.state.article.comment_count}</p>
+          <h3>{title}</h3>
+          <p>topic: {topic}</p>
+          <p>body: {body}</p>
+          <p>author: {author}</p>
+          <p>
+            <button
+              id="up"
+              onClick={() => {
+                return this.handleArticleClick(1, article_id, 'article');
+              }}
+              disabled={this.state.vote_inc === 1}
+            >
+              {' '}
+              Up{' '}
+            </button>{' '}
+            votes: {votes + this.state.vote_inc}{' '}
+            <button
+              id="down"
+              onClick={() => {
+                return this.handleArticleClick(-1, article_id, 'article');
+              }}
+              disabled={this.state.vote_inc === -1}
+            >
+              {' '}
+              Down{' '}
+            </button>
+          </p>
+          <p>comments: {comment_count}</p>
         </div>
         <div className="comments">
           <div className="addComment">place holder for add comments</div>
           {this.state.comments.map(comment => (
-            <div key="comment.comment_id" className="containerComment">
+            <div key={comment.comment_id} className="containerComment">
               <p className="author">{comment.author}</p>
               <p className="body">{comment.body}</p>
               <p className="created">
                 {Date(comment.created_at).slice(0, -34)}
               </p>
-              <p className="votes">votes</p>
+              <p className="votes">
+                <button
+                  id="up"
+                  onClick={() => {
+                    return this.handleCommentClick(
+                      1,
+                      comment.comment_id,
+                      'comment'
+                    );
+                  }}
+                  disabled={this.state[comment.comment_id] === 1}
+                >
+                  {' '}
+                  Up{' '}
+                </button>{' '}
+                votes:{' '}
+                {this.state[comment.comment_id]
+                  ? comment.votes + this.state[comment.comment_id]
+                  : comment.votes + 0}{' '}
+                <button
+                  id="down"
+                  onClick={() => {
+                    return this.handleCommentClick(
+                      -1,
+                      comment.comment_id,
+                      'comment'
+                    );
+                  }}
+                  disabled={this.state[comment.comment_id] === -1}
+                >
+                  {' '}
+                  Down{' '}
+                </button>
+              </p>
             </div>
           ))}
         </div>
@@ -38,12 +104,31 @@ class Article extends Component {
     await this.fetchArticleContent();
   };
 
+  handleArticleClick = (inc, id, comp) => {
+    this.setState(state => {
+      return {
+        article: state.article,
+        comments: state.comments,
+        vote_inc: state.vote_inc + inc
+      };
+    });
+    api.updateVotes(inc, id, comp);
+  };
+  handleCommentClick = (inc, id, comp) => {
+    this.setState(state => {
+      return {
+        article: state.article,
+        comments: state.comments,
+        [id]: state[id] ? state[id] + inc : inc
+      };
+    });
+    api.updateVotes(inc, id, comp);
+  };
+
   fetchArticleContent = async () => {
     try {
       const article = await api.getArticle(this.props.article);
-      console.log(article, '<< article data');
       const articleComments = await api.getArticleComments(this.props.article);
-      console.log(articleComments, '<< articleComments data');
 
       this.setState(state => {
         return {
